b9a650d45a1d82d496ee296bbdd63d5e1541e484
Isaias123Bv <3521110250@uth.edu.mx>
2022-09-29 20:27:44 -0500

    Update login.php
    ca

--- login.php
+++ login.php
@@ -4,9 +4,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+
 </head>
 <body>
-    
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <p align="right">
+                <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Salir</a>
+                </li>
+            </ul></p>
+        </div>
+    </div>
+</div>
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3"></div>
+			<div class="col-md-6">
+				
+				<p align="center"><img src="imagenes/usuario.png"></p>
+				<form action="validar.php" name="login" method="get">
+					
+					<div class="form-group">
+						<label for="usuario">Usuario</label>
+						<input type="email" class="form-control" id="usuario" name="usuario" placeholder="ejemplo@hotmail.com" required>
+					</div>
+
+					<div class="form-group">
+						<label for="clave">Clave</label>
+						<input type="password" class="form-control" id="clave" name="clave" maxlength="10" required>
+					</div>
+
+					<p align="center"><input type="submit" class="btn btn-primary" name="enviar" value="Loguearse"></p>
+
+				</form>
+				<center>
+
+					<a href="recuperar.php">¿Olvidaste tu contraseña?</a> <br>
+					<a href="index.php">Inicio</a>
+				</center>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
